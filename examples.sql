96f5916498ee8a87312b479862d71fa82557e764
Alladio Bonesso <Alladio@users.noreply.github.com>
2023-04-27 10:11:45 -0300

    Row_number + Dense_rank

--- examples.sql
+++ examples.sql
@@ -12,4 +12,21 @@ SELECT produto_nome, produto_categoria_nome AS Categoria,
         COUNT(produto_id) OVER(PARTITION BY product_category_name) AS Total_Produtos
 FROM produtos
 
+--OU, com: DISTINCT
+
+SELECT produto_nome, produto_categoria_nome AS Categoria,
+        COUNT(produto_id) OVER(PARTITION BY product_category_name) AS Total_Produtos
+FROM produtos
+
+
 -- UTILIZANDO ROW NUMBER
+
+SELECT produto_nome, 
+        produto_id,
+        produto_categoria_nome AS Categoria,
+        produto_peso AS Peso_do_Produto,
+        ROW_NUMBER() OVER(PARTITION BY produto_categoria_nome ORDER BY produto_peso) AS Numero_da_Linha,
+        DENSE_RANK() OVER(PARTITION BY produto_categoria_nome ORDER BY produto_peso) AS Rank_da_Linha,
+FROM produtos
+WHERE produto_categoria_nome = 'bicicletas'
+ORDER BY produto_peso DESC
